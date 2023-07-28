7a89bb4a7ee3c937f21bdce20ad48375d964afe8
kachow4 <kchow1sfo@gmail.com>
2023-07-28 01:03:42 -0700

    add active navbar tabs

--- components/core/Navbar.tsx
+++ components/core/Navbar.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+  const currentRoute = usePathname();
   return (
     <div className="flex px-24 py-5 justify-between">
         <div className="logo-wrapper justify-start">
@@ -17,19 +20,19 @@ export default function Navbar() {
         <div className="tabs-wrapper pt-6">
             <ul className="tabs flex gap-10 justify-end">
                 <li className="hover:underline">
-                  <Link href="/">HOME</Link>
+                  <Link href="/" className={currentRoute === "/" ? "underline" : ""}>HOME</Link>
                 </li>
                 <li className="hover:underline">
-                  <Link href="/about-us">ABOUT US</Link>
+                  <Link href="/about-us" className={currentRoute === "/about-us" ? "underline" : ""}>ABOUT US</Link>
                 </li>
                 <li className="hover:underline">
-                  <Link href='/team'>MEET THE TEAM</Link>
+                  <Link href='/team' className={currentRoute === "/team" ? "underline" : ""}>MEET THE TEAM</Link>
                 </li>
                 <li className="hover:underline">
                   <a target="_blank" href="https://linktr.ee/urmouci" rel="noopener noreferrer">RESOURCES</a>
                 </li>
                 <li className="hover:underline">
-                  <Link href='/contact-us'>CONTACT US</Link>
+                  <Link href='/contact-us' className={currentRoute === "/contact-us" ? "underline" : ""}>CONTACT US</Link>
                 </li>
             </ul>
         </div>
